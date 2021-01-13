5e56349b926639c3f38825f8a26ba424ae1de137
Joakim Teixeira <setokim1@hotmail.com>
2021-01-13 19:57:57 -0300

    Fix styling

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,15 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Login, Register } from 'pages/auth';
 import Home from 'pages/Home';
 import NavBar from 'components/NavBar';
+import { Container } from 'react-bootstrap';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <NavBar />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        <Container className="col-md-6 mt-5">
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Container>
       </Switch>
     </BrowserRouter>
   );
